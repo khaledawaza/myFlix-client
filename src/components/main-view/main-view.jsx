aee935fbc0a93067b0069a826cc8b01ad43ada96
khaledawaza <1khaledawaza@gmail.com>
2022-11-23 02:04:34 +0100

    add a back button and title

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
 
 export class MainView extends React.Component {
 
@@ -10,15 +11,52 @@ export class MainView extends React.Component {
       movies: [{title: "Clash of Titans"}, {title: "Guardians of the Galaxy"}]
     }
   }
-
+  setSelectedMovie(newSelectedMovie) {
+    this.setState({
+      selectedMovie: newSelectedMovie
+    });
+  }
   render() {
-    const { movies } = this.state;
-  
+    const { movies, selectedMovie } = this.state;
+
+
     if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
-  
+
     return (
       <div className="main-view">
-        {movies.map((movieItem, index) => <MovieCard movie={movieItem} key={index} />)}
+        {selectedMovie
+          ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+          : movies.map(movie => (
+            <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
+          ))
+        }
+      </div>
+    );
+  }
+}
+
+import React from 'react';
+
+export class MovieView extends React.Component {
+  
+  render() {
+    const { movie, onBackClick } = this.props;
+
+    return (
+      <div className="movie-view">
+        <div className="movie-poster">
+          <img src={movie.ImagePath} />
+        </div>
+        <div className="movie-title">
+          <span className="label">Title: </span>
+          <span className="value">{movie.Title}</span>
+        </div>
+        <div className="movie-description">
+          <span className="label">Description: </span>
+          <span className="value">{movie.Description}</span>
+        </div>
+        <button onClick={() => { onBackClick(null); }}>Back</button>
+
       </div>
     );
   }
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,7 +3,33 @@ import React from 'react';
 export class MovieCard extends React.Component {
   render() {
     const {movie} = this.props;
-    
-    return <div className="movie-card">{movie.title}</div>;
+    return (
+      <div className="movie-view">
+        <div className="movie-poster">
+          <img src={movie.ImagePath} />
+        </div>
+        <div className="movie-title">
+          <span className="label">Title: </span>
+          <span className="value">{movie.Title}</span>
+        </div>
+        <div className="movie-description">
+          <span className="label">Description: </span>
+          <span className="value">{movie.Description}</span>
+        </div>
+        <button>Back</button>
+
+       </div>
+    );
+  }
+}
+ 
+   
+import React from 'react';
+
+export class MovieCard extends React.Component {
+  render() {
+    const { movie, onMovieClick } = this.props;
+
+    return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
   }
 }
