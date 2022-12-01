312cc0c7aab7127c164b6743a8a01a1a209d6be4
khaledawaza <1khaledawaza@gmail.com>
2022-12-01 14:29:21 +0100

    add a new fils and same change with movies ditils

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -6,11 +8,25 @@ export class MainView extends React.Component {
 
   constructor(){
     super();
-
+  
     this.state = {
       movies: [{title: "Clash of Titans"}, {title: "Guardians of the Galaxy"}]
-    }
+     }
   }
+
+
+  componentDidMount(){
+    axios.get('https://[APP-NAME].herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   setSelectedMovie(newSelectedMovie) {
     this.setState({
       selectedMovie: newSelectedMovie
@@ -19,44 +35,15 @@ export class MainView extends React.Component {
   render() {
     const { movies, selectedMovie } = this.state;
 
-
     if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
-
     return (
       <div className="main-view">
         {selectedMovie
           ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
           : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
-          ))
+            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
+         ))
         }
-      </div>
-    );
-  }
-}
-
-import React from 'react';
-
-export class MovieView extends React.Component {
-  
-  render() {
-    const { movie, onBackClick } = this.props;
-
-    return (
-      <div className="movie-view">
-        <div className="movie-poster">
-          <img src={movie.ImagePath} />
-        </div>
-        <div className="movie-title">
-          <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
-        </div>
-        <div className="movie-description">
-          <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
-        </div>
-        <button onClick={() => { onBackClick(null); }}>Back</button>
-
       </div>
     );
   }
