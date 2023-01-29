45f29d317e6ebf263e01eed446b94841cac7747d
khaledawaza <105608383+khaledawaza@users.noreply.github.com>
2023-01-30 00:25:13 +0100

    fix some err

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,119 +1,258 @@
-import { useState, useEffect } from "react";
-import { BookCard } from "../movie-card/movie-card";
-import { BookView } from "../movie-view/movie-view";
-import { LoginView } from "../login-view/login-view";
-import Row from "react-bootstrap/Row";
-import{ useState } from "react";
+import React from 'react';
+import axios from 'axios';
+import { Row, Col, Container } from 'react-bootstrap';
+
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+
+import './main-view.css';         
+import { Menubar } from '../menubar/menubar';
+import { RegistrationView } from '../registration-view/registration-view';
+import { LoginView } from '../login-view/login-view';
+import { MovieView } from '../movie-view/movie-view';
+import { DirectorView } from '../director-view/director-view';
+import { ProfileView } from '../profile-view/profile-view';
+import { UserUpdate } from '../profile-view/user-update';
+import { GenreView } from '../genre-view/genre-view';
+
+// connect func help us to connect any component within the application to the store
+import { connect } from 'react-redux';
+
+// importing relevant action, here is setMovies and will be used in render() to connect to MainView using connect()
+import { getMovies } from '../../actions/actions';
+
+import MoviesList from '../movies-list/movies-list';
 
 
-export const MainView = () => {
-  const [books, setBooks] = useState([]);
-  const [selectedBook, setSelectedBook] = useState(null);
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    fetch("https://openlibrary.org/search.json?q=star+wars")
-      .then((response) => response.json())
-      .then((data) => {
-        const booksFromApi = data.docs.map((doc) => {
-          return {
-            id: doc.key,
-            title: doc.title,
-            image: `https://covers.openlibrary.org/b/id/${doc.cover_i}-L.jpg`,
-            author: doc.author_name?.[0]
-          };
-        });
-
-        setBooks(booksFromApi);
+// Making and exposing the MainView component in order to be usable in other files using the React.Component template
+export class MainView extends React.Component {
+  constructor() {
+    super();
+    // initial state is set to null
+    this.state = {
+      movies: [],
+      user: "",
+    };
+  }
+  // updated the method to persist the login data
+  componentDidMount() {
+    // first getting the value of the token from the local storage
+    let accessToken = localStorage.getItem('token');
+    if (accessToken !== null) {
+      this.setState({
+        user: JSON.parse(localStorage.getItem('User')),
       });
-  }, []);
+      // if the user is logged in then the getMovies method will be called (makes GET request to the movies endpoint)
+      this.getMovies(accessToken);
+    }
+  }
+  onLoggedIn(authData) {
+    const userObj = {
+      name: authData.user.Username,
+      userID: authData.user._id,
+      favoriteMovies: authData.user.FavoriteMovies,
+    };
+    this.setState({
+      user: userObj,
+    });
 
-  return (
-      <Row> 
-        {!user ? (
-          <>
-            <LoginView onLoggedIn={(user) => setUser(user)} />
-            or
-            <SignupView />
-          </>
-        ) : selectedBook ? (
-          <BookView 
-            book={selectedBook} 
-            onBackClick={() => setSelectedBook(null)} 
-          />
-        ) : books.length === 0 ? (
-          <div>The list is empty!</div>
-        ) : (
-          <>
-          {books.map((book) => (
-            <Col className="mb-5" key={book.id} md={3}>
-              <BookCard
-                book={book}
-                onBookClick={(newSelectedBook) => {
-                  setSelectedBook(newSelectedBook);
-                }}
-              />
-            </Col>
-          ))}
-        </>
-        )}
-      </Row>
-  );
-};
+    localStorage.setItem('token', authData.token);
+    localStorage.setItem('User', JSON.stringify(userObj));
+    this.getMovies(authData.token);
+  }
 
-export const SignupView = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [birthday, setBirthday] = useState("");
+  // adding a new GetMovie method in order to be able to make authenticated requests to the API and see the list of movies
+  getMovies(token) {
+    axios
+      .get('https://movie-api.fly.dev/movies', {
+        // passing the bearer authorization in the header of the http request to make authenticated request to te API
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        // assigning the result to the state
+        this.setState({
+          movies: response.data,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
-  const handleSubmit = (event) => {};
+  // add movie to fave list
+  addMovie(movieId) {
+    axios({
+      method: 'post',
+      url: `https://movie-api.fly.dev/users/${this.state.user.userID}/movies/${movieId}`,
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-          minLength="3"
-        />
-      </label>
-      <label>
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Email:
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </label>
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+    })
+      .then((response) => {
+        alert('The movie was successfully added.');
+      })
+      .catch((err) => console.log(err));
+  }
 
-      <label>
-        Birthday:
-        <input
-          type="date"
-          value={birthday}
-          onChange={(e) => setBirthday(e.target.value)}
-          required
-        />
-      </label>
-      
-      <button type="submit">Submit</button>
-    </form>
-  );
-};
+  // delete movie from fav list
+  deleteMovie(movieId) {
+    axios({
+      method: 'delete',
+      url: `https://movie-api.fly.dev/users/${this.state.user.userID}/movies/${movieId}`,
 
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+    })
+      .then((response) => {
+        alert('The movie was successfully removed.');
+      })
+      .catch((err) => console.log(err));
+  }
 
+  // Rendering the visual representation of the component
+  render() {
+    const { movies } = this.props;
+    const { user } = this.state;
 
+    return (
+      <Router>
+        {/* placed Menubar */}
+        <Menubar user={user} />
+        <Container>
+          <Row className="main-view justify-content-md-center mt-3">
+            {/* Routing starts from here */}
+            <Route
+              exact
+              path="/"
+              render={() => {
+                /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+                if (!user)
+                  return (
+                    <Col>
+                      <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />
+                    </Col>
+                  );
+                // before the movies have been loaded
+                if (movies.length === 0) return <div className="main-view" />;
+                // MoviesList receives the movies from the store in stages
+              return <MoviesList movies={movies} />;
+              }}
+            />
+            {/* creatinhg route for Menubar view */}
+            <Route
+              path="/register"
+              render={() => {
+                if (user) return <Redirect to="/" />;
+                return (
+                  <Col>
+                    <RegistrationView />
+                  </Col>
+                );
+              }}
+            />
+            <Route
+              path="/movies/:movieId"
+              render={({ match, history }) => {
+                {
+                  /* check if user has the auth to open the link */
+                }
+                if (!user)
+                  return (
+                    <Col>
+                      <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />
+                      ;
+                    </Col>
+                  );
+                if (!movies) return <div className="main-view" />;
+                return (
+                  <Col md={8}>
+                    <MovieView
+                      movie={movies.find((m) => m._id === match.params.movieId)}
+                      onBackClick={() => history.goBack()}
+                      addMovie={(movieID) => this.addMovie(movieID)}
+                      removeMovie={(movieID) => this.deleteMovie(movieID)}
+                      user={user}
+                    />
+                  </Col>
+                );
+              }}
+            />
+            <Route
+              path="/directors/:name"
+              render={({ match, history }) => {
+                if (!user)
+                  return (
+                    <Col>
+                      <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />
+                    </Col>
+                  );
+                if (!movies) return <div className="main-view" />;
+                return (
+                  <Col md={8}>
+                    <DirectorView
+                      director={
+                        movies.find(
+                          (m) => m.Director.Name === match.params.name
+                        ).Director
+                      }
+                      directedMovies={movies.filter(
+                        (m) => m.Director.Name === match.params.name
+                      )}
+                      onBackClick={() => history.goBack()}
+                    />
+                  </Col>
+                );
+              }}
+            />
+            <Route
+              path="/genres/:name"
+              render={({ match, history }) => {
+                if (!user)
+                  return (
+                    <Col>
+                      <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />
+                    </Col>
+                  );
+                if (!movies) return <div className="main-view" />;
+                return (
+                  <Col md={8}>
+                    <GenreView
+                      genre={
+                        movies.find((m) => m.Genre.Name === match.params.name)
+                          .Genre
+                      }
+                      genreMovies={movies.filter(
+                        (m) => m.Genre.Name === match.params.name
+                      )}
+                      onBackClick={() => history.goBack()}
+                    />
+                  </Col>
+                );
+              }}
+            />
+            <Route
+              path={`/users/:username`}
+              render={({ match, history }) => {
+                if (user.name != match.params.username)
+                  return <Redirect to="/" />;
+                return (
+                  <Col>
+                    <ProfileView
+                      movies={movies}
+                      user={user}
+                      onBackClick={() => history.goBack()}
+                      removeFavorites={(movieId) => this.deleteMovie(movieId)}
+                    />
+                  </Col>
+                );
+              }}
+            />
+          </Row>
+        </Container>
+      </Router>
+    );
+  }
+}
+let mapStateToProps = state => {
+  return { movies: state.movies }
+}
+
+export default connect(mapStateToProps, { getMovies })(MainView);
+//
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,34 +1,99 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Card, CardGroup, Container, Col, Row } from 'react-bootstrap';
+import { Button, Container, Col, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './movie-view.css';
 
-import './movie-card.css';
+export class MovieView extends React.Component {
+  keypressCallback(event) {
+    console.log(event.key);
+  }
 
-export class MovieCard extends React.Component {
+  componentDidMount() {
+    document.addEventListener('keypress', this.keypressCallback);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keypress', this.keypressCallback);
+  }
+
   render() {
-    const { movie } = this.props;
+    const { movie, onBackClick, removeMovie, addMovie, user } = this.props;
 
+      console.log(movie);
     return (
-      <Container fluid className="movieCardContainer">
+      <Container fluid className="movieViewContainer">
         <Row>
           <Col>
-            <CardGroup>
-              <Card className="movieCard mt-3 mb-3">
-                <Card.Img variant="top" src={movie.Image} crossOrigin="anonymous"/>
-                <Card.Body>
-                  <Card.Title>{movie.Title}</Card.Title>
-                  <Card.Text className="text-truncate">
-                    {movie.Summary}
-                  </Card.Text>
-                  <Link to={`/movies/${movie._id}`}>
-                    <Button variant='outline-warning'>
-                      Open
-                    </Button>
-                  </Link>
-                </Card.Body>
-              </Card>
-            </CardGroup>
+            <img
+              className="movie-poster"
+              crossOrigin="anonymous"
+              src={movie.Image}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className="movie-title">
+              <span className="label">Title: </span>
+              <span className="value">{movie.Title}</span>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className="movie-Summary">
+              <span className="label">Summary: </span>
+              <span className="value">{movie.Summary}</span>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className="movie-releaseyear">
+              <span className="label">ReleaseYear: </span>
+              <span className="value">{movie.Year}</span>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className="movie-director">
+              <Link to={`/directors/${movie.Director.Name}`}>
+                <Button variant="link">Director</Button>
+              </Link>
+            </div>
+            <div>{`Bio: ${movie.Director.Bio}`}</div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className="movie-genre">
+              <Link to={`/genres/${movie.Genre.Name}`}>
+                <Button variant="link">Genre: {movie.Genre.Name}</Button>
+              </Link>
+            </div>
+            <div>{`Description: ${movie.Genre.Description}`}</div>
+            <Button
+              onClick={() => {
+                onBackClick();
+              }}
+            >
+              Back
+            </Button>
+            {!user.favoriteMovies.includes(movie._id) && 
+              <Button onClick={() => addMovie(movie._id)} className="ml-2 my-2">
+                Add to Favorites.
+              </Button>
+            }
+            {user.favoriteMovies.includes(movie._id) && 
+              <Button
+                onClick={() => removeMovie(movie._id)}
+                className="ml-2 my-2"
+              >
+                Remove from Favorites.
+              </Button>
+            }
           </Col>
         </Row>
       </Container>
@@ -36,23 +101,23 @@ export class MovieCard extends React.Component {
   }
 }
 
-MovieCard.propTypes = {
+MovieView.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
     Genre: PropTypes.shape({
       Name: PropTypes.string.isRequired,
-      Description: PropTypes.string
+      Description: PropTypes.string,
     }),
     Summary: PropTypes.string.isRequired,
     Director: PropTypes.shape({
       Name: PropTypes.string.isRequired,
       Bio: PropTypes.string.isRequired,
-      Image: PropTypes.string.isRequired
+      Image: PropTypes.string.isRequired,
     }),
     Actors: PropTypes.array.isRequired,
     Image: PropTypes.string.isRequired,
     Year: PropTypes.number.isRequired,
-    Featured: PropTypes.bool
+    Featured: PropTypes.bool,
   }).isRequired,
-  onMovieClick: PropTypes.func
+  onBackClick: PropTypes.func.isRequired,
 };
