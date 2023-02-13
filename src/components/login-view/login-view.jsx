27a836c6e55d88fb586d4dc9f993acbd272eb887
khaledawaza <105608383+khaledawaza@users.noreply.github.com>
2023-02-13 11:28:37 +0100

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,92 +1,101 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import { MovieCard } from "../movie-card/movie-card";
-import { MainView } from "../main-view/main-view";
-import { LoginView } from "../login-view/login-view";
+import axios from 'axios';
+import { useState } from 'react';
+import {
+  Button,
+  Form,
+  Card,
+  CardGroup,
+  Container,
+  Col,
+  Row,
+} from 'react-bootstrap';
+import CardHeader from 'react-bootstrap/esm/CardHeader';
 
-export const MainView = () => {
-  const [books, setBooks] = useState([]);
-  const [selectedBook, setSelectedBook] = useState(null);
-  const [user, setUser] = useState(null);
+export const LoginView = ({ onLoggedIn }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-  useEffect(() => {
-    fetch("https://openlibrary.org/search.json?q=star+wars")
-      .then((response) => response.json())
-      .then((data) => {
-        const booksFromApi = data.docs.map((doc) => {
-          return {
-            id: doc.key,
-            title: doc.title,
-            image: `https://covers.openlibrary.org/b/id/${doc.cover_i}-L.jpg`,
-            author: doc.author_name?.[0]
-          };
-        });
-
-        setBooks(booksFromApi);
-      });
-  }, []);
-
-
-
-  if (!user) {
-    return <LoginView />;
-  }
-
-  if (selectedBook) {
-    return (
-      <BookView book={selectedBook} onBackClick={() => setSelectedBook(null)} />
-    );
-  }
-
-  if (books.length === 0) {
-    return <div>The list is empty!</div>;
-  }
-
-  return (
-    <div>
-      {books.map((book) => (
-        <BookCard
-          key={book.id}
-          book={book}
-          onBookClick={(newSelectedBook) => {
-            setSelectedBook(newSelectedBook);
-          }}
-        />
-      ))}
-    </div>
-  );
-};
-
-
-export const LoginView = () => {
   const handleSubmit = (event) => {
     // this prevents the default behavior of the form which is to reload the entire page
     event.preventDefault();
 
     const data = {
-      access: username,
-      secret: password
+      Username: username,
+      Password: password,
     };
 
-    fetch("https://openlibrary.org/account/login.json", {
-      method: "POST",
-      body: JSON.stringify(data)
-    });
-  };
+    axios.post('http://localhost:8080/login', data).then(res => console.log(res))
 
+     fetch('https://myflixapi-50jx.onrender.com/login', {
+       method: "POST",
+       body: JSON.stringify(data),
+       headers: {
+         "Content-Type": "application/json",
+         "Access-Control-Allow-Origin": "*"
+       }
+     })
+     .then((response) => response.json())
+     .then((data) => {
+       console.log("Login response: ", data);
+       if (data.user) {
+         localStorage.setItem("user", JSON.stringify(data.user));
+         localStorage.setItem("token", data.token);
+         onLoggedIn(data.user, data.token);
+       } else {
+         alert("No such user");
+       }
+     })
+     .catch((e) => {
+       alert("Something went wrong");
+     });
+  };
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input type="text" />
-      </label>
-      <label>
-        Password:
-        <input type="password" />
-      </label>
-      <button type="submit">
-        Submit
-      </button>
-    </form>
+    <Container>
+      {/* <EntranceGreating /> */}
+      <Row>
+        <Col>
+          <CardGroup>
+            <Card className='border-0'>
+              <Card.Body>
+                <Form onSubmit={handleSubmit}>
+                  <Form.Group controlId='formUsername' className='mt-2'>
+                    <Form.Label>Username:</Form.Label>
+                    <Form.Control
+                      type='text'
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                      required
+                      minLength='3'
+                      pattern="^[A-Za-z0-9 .,'\-!?%&]+$"
+                      title="Username should contain more than 3 characters, may only contain letters, numbers and special characters: .,'-!?%&"
+                      placeholder='Enter your name'
+                    />
+                  </Form.Group>
+                  <Form.Group controlId='formPassword' className='mt-3'>
+                    <Form.Label>Password:</Form.Label>
+                    <Form.Control
+                      type='password'
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      required
+                      pattern="^[A-Za-z0-9 .,'\-!?%&]+$"
+                      title="Password may only contain letters, numbers and special characters: .,'-!?%&"
+                      placeholder='Enter your password'
+                    />
+                  </Form.Group>
+                  <Row>
+                    <Col className='text-end'>
+                      <Button variant='primary' type='submit' className='mt-3'>
+                        Submit
+                      </Button>
+                    </Col>
+                  </Row>
+                </Form>
+              </Card.Body>
+            </Card>
+          </CardGroup>
+        </Col>
+      </Row>
+    </Container>
   );
 };
