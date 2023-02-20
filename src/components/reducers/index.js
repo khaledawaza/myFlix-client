19ac673ae42807066f1be1d262d685ccec222e0a
khaledawaza <1khaledawaza@gmail.com>
2023-02-20 22:35:27 +0100

    add rodex and reducers folder

---
+++ src/components/reducers/index.js
@@ -0,0 +1,28 @@
+import {
+    ADD_TODO,
+    TOGGLE_TODO
+  } from '../actions';
+
+  const todos = (state = [], action) => {
+   switch(action.type) {
+    case ADD_TODO:
+        return[
+            ...state,
+            {
+                Text:action.Text,
+                completed:false
+            }
+        ];
+        case TOGGLE_TODO:
+            return state.map((todo, index) =>
+                (index === action.index)
+                ? {...todo, completed: !todo.completed}
+                : todo
+            );
+            default:
+            return state
+   }
+  };
+  export default todos;
+
+ 
