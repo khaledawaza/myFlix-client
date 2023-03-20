4bf743750dc552278d28c722cadf10b4fa217c1c
khaledawaza <1khaledawaza@gmail.com>
2023-03-20 23:21:57 +0100

    update all component imports in main view

--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,19 @@
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 
 // Importing MainView
-import { MainView } from './components/main-view/main-view';
+import { MainView } from "./components/main-view/main-view";
 
 // Import Bootstrap react
-import Container from 'react-bootstrap/Container';
+import Container from "react-bootstrap/Container";
 
-import './index.css';
+import "./index.css";
 
 const MyFlixApplication = () => {
-  return (
-
-      <MainView />
-
-  );
+  return <MainView />;
   // <MainView />;
 };
-
 // Finds the root of your app
-const container = document.querySelector('#root');
+const container = document.querySelector("#root");
 const root = createRoot(container);
 
 // Tells React to render your app in the root DOM element
